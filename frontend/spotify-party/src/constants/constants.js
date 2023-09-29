ed28879bf4f5e0b402055b523c5146b5ccd67c7b
stanley.chan <stanley.chan185@gmail.com>
2023-09-28 23:47:39 -0400

    updates to redirect and api urls

--- frontend/spotify-party/src/constants/constants.js
+++ frontend/spotify-party/src/constants/constants.js
@@ -5,7 +5,7 @@ const constants = {
     REACT_APP_STORAGE_BUCKET: "group-spotify.appspot.com",
     REACT_APP_MESSAGING_SENDER_ID: "616970081853",
     REACT_APP_APP_ID: "1:616970081853:web:88764edaffd49f2a723c54",
-    SERVER_URL: "https://partify-it.netlify.app/api"
+    SERVER_URL: "https://partify-it.netlify.app/.netlify/functions/api"
 }
 
 export default constants;
